687e480524537f5ca9c19597700cb725eeede880
Kazuki Okamoto <kiki.com.start@gmail.com>
2020-10-06 20:06:44 +0900

    セッションが切れた場合に/signinに飛ばす

--- frontend/vue-demo/src/api.js
+++ frontend/vue-demo/src/api.js
@@ -1,10 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
+import router from "./router";
 
-const baseURL = process.env.VUE_APP_API_BASE_URL
+const baseURL = process.env.VUE_APP_API_BASE_URL;
 
-export default axios.create({
-    baseURL,
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-    },
-})
+const api = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+  },
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      router.push("/signin").catch((err) => console.log(err));
+    }
+    return error;
+  }
+);
+
+export default api;
